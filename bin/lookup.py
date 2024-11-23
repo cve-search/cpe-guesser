22fd8e122ec57a3cb4b2558dd34a17096b70d6af
Alexandre Dulaunoy <a@foo.be>
2024-11-23 16:59:36 +0100

    chg: [lookup] add `--unique` option to return the best matching CPE

--- bin/lookup.py
+++ bin/lookup.py
@@ -21,7 +21,21 @@ if __name__ == "__main__":
         nargs="+",
         help="One or more keyword(s) to lookup",
     )
+    parser.add_argument(
+        "--unique",
+        action="store_true",
+        help="Return the best CPE matching the keywords given",
+        default=False,
+    )
     args = parser.parse_args()
 
     cpeGuesser = CPEGuesser()
-    print(json.dumps(cpeGuesser.guessCpe(args.word)))
+    r = cpeGuesser.guessCpe(args.word)
+    if not args.unique:
+        print(json.dumps(r))
+    else:
+        try:
+            r = r[:1][0][1]
+        except:
+            r = []
+        print(json.dumps(r))
